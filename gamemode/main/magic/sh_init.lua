83f984f8a717cc7c65b00d18f56d6738d6b9ee68
Nyaaa <5182757+oNyaaa1@users.noreply.github.com>
2025-09-07 19:18:56 +0100

    update

--- gamemode/main/magic/sh_init.lua
+++ gamemode/main/magic/sh_init.lua
@@ -1,1 +1,9 @@
 print("Magic Loaded")
+local meta = FindMetaTable("Player")
+function meta:Charge(mana)
+    self:SetNWFloat("Rust_Mana", mana or 0)
+end
+
+function meta:GetCharge(mana)
+    return self:GetNWFloat("Rust_Mana", mana or 0)
+end
